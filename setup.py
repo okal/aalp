30996e5164248c425df248651d2ecfd20bef821f
Okal Otieno <okal@justokal.com>
2013-08-02 17:19:31 +0300

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aapl',
+    version='0.1',
+    description='Basic Apache Access Log Parser',
+    author='Okal Otieno',
+    author_email='okal+python@justokal.com',
+    url='https://github.com/okal/aapl',
+    packages=find_packages(),
+    license='MIT License',
+    zip_safe=True)
