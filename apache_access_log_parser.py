670dbc7a8e5c7af574b899000d875f844a377274
Okal Otieno <okal@justokal.com>
2013-08-02 14:05:39 +0300

    Change params so that functions other than parse and tokenize accept only a log entry list\n Remove comments

--- apache_access_log_parser.py
+++ apache_access_log_parser.py
@@ -13,51 +13,30 @@ access_log_field_names = [
 ]
 
 def tokenize(access_log_data):
-    'Returns a list of strings for each access log entry'
     cleaned_access_log_data = access_log_data.replace('[', '"').replace(']', '"')
     return [ shlex.split(entry) for entry in cleaned_access_log_data.splitlines() ]
 
 def get_tree(log_entry_list):
-    """
-    Returns a list of dictionaries each representing individual log entries
-    """
     log_entry_dict_list = [dict(zip(access_log_field_names, entry)) for entry in log_entry_list]
     log_entry_dict_list.sort(lambda x, y: cmp(x['client-ip'], y['client-ip']))
     return log_entry_dict_list
 
 def parse(access_log_data):
-    """
-    Returns a list of dictionaries each representing individual log entries
-    given the contents of an access log.
-    """
     return get_tree(tokenize(access_log_data))
 
-def filter_by_property_value(access_log_data, access_log_property, value):
-    """
-    Returns a list of dictionaries each representing individual log entries
-    filtered by the value of a given property.
-    
-    Possible properties are:
-    
-    "client-ip", "ident", username", "time", "request-line", "status-code",
-    "size", "referrer" and "agent"
+def filter_by_property_value(log_entry_dict_list, access_log_property, value):
+    return [entry for entry in log_entry_dict_list if entry[access_log_property] == value]
 
-    """
-    return [entry for entry in parse(access_log_data) if entry[access_log_property] == value]
+def filter_by_client_ip(log_entry_dict_list, client_ip):
+    return filter_by_property_value(log_entry_dict_list, 'client-ip', client_ip)
 
-def filter_by_client_ip(access_log_data, client_ip):
-    return filter_by_property_value(access_log_data, 'client-ip', client_ip)
-
-def filter_by_GET_URL(access_log_data, url):
+def filter_by_GET_URL(log_entry_dict_list, url):
     request_line = 'GET %s HTTP/1.1' % url
-    return filter_by_property_value(access_log_data, 'request-line', request_line)
+    return filter_by_property_value(log_entry_dict_list, 'request-line', request_line)
 
-def get_all_property_values(access_log_data, property_key):
-    """
-    Get all values of a given property of log entries given the property key.
-    """
-    return [entry[property_key] for entry in parse(access_log_data)]
+def get_all_property_values(log_entry_dict_list, property_key):
+    return [entry[property_key] for entry in log_entry_dict_list]
 
-def get_all_client_IPs(access_log_data, client_ip):
-    return set(get_all_property_values(access_log_data, 'client-ip'))
+def get_all_client_IPs(log_entry_dict_list, client_ip):
+    return set(get_all_property_values(log_entry_dict_list, 'client-ip'))
 
